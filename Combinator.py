6078f76e1bbb49e62e7d24f7b7bb0ef3ebe13973
Sr. Meia-noiteλ <lordzarkares@Outlook.com>
2022-11-23 10:41:10 -0300

    |

---
+++ Combinator.py
@@ -0,0 +1,27 @@
+from Curry import Curry
+
+@Curry
+def Id(a):
+    """:: a -> a"""
+    return a
+
+@Curry
+def Const(a, b):
+    """:: a -> b -> a"""
+    return a
+
+@Curry
+def Uncurry(function, a_and_b):
+    """:: (a -> b -> c) -> (a, b) -> c"""
+    a, b = a_and_b
+    return function& a& b
+
+@Curry
+def Substitute(binary, unary, a):
+    """:: (a -> b -> c) -> (a -> b) -> a -> c"""
+    return binary& a& (unary& a)
+
+@Curry
+def Inbetween(a, b, function):
+    """:: a -> b -> (a -> b -> c) -> c"""
+    return function& a& b
