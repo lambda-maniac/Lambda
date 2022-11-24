b321b4e80cf6da9669d2440e702d5e85ea7b12ae
Sr. Meia-noiteλ <lordzarkares@Outlook.com>
2022-11-24 16:42:42 -0300

    |

--- Combinator.py
+++ Combinator.py
@@ -1,37 +1,37 @@
 from Curry import Curry
 
 @Curry
-def Id(a):
+def id(a):
     """:: a -> a"""
     return a
 
 @Curry
-def Const(a, b):
+def const(a, b):
     """:: a -> b -> a"""
     return a
 
 @Curry
-def Flip(function, b, a):
+def flip(function, a, b):
     """:: (a -> b -> c) -> b -> a -> c"""
-    return function& a& b
+    return function& b& a
 
 @Curry
-def Uncurry(function, a_and_b):
+def uncurry(function, a_and_b):
     """:: (a -> b -> c) -> (a, b) -> c"""
     a, b = a_and_b
     return function& a& b
 
 @Curry
-def Substitute(binary, unary, a):
+def amalgamation(binary, unary, a):
     """:: (a -> b -> c) -> (a -> b) -> a -> c"""
     return binary& a& (unary& a)
 
 @Curry
-def Inbetween(a, b, function):
+def inbetween(a, b, function):
     """:: a -> b -> (a -> b -> c) -> c"""
     return function& a& b
 
 @Curry
-def Compose(f, g, a):
+def compose(f, g, a):
     """:: (a -> b) -> (b -> c) -> a -> c"""
     return f& (g& a)
